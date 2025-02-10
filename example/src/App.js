6e9e3cf8fa06c9a14ffafb26ad5b9934da5458ef
neo chatelin <neo.chatelin@gmail.com>
2025-02-10 14:31:09 +0100

    _

--- example/src/App.js
+++ example/src/App.js
@@ -1,10 +1,31 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-import { ExampleComponent } from 'react-rails-pagination'
+import { ExampleComponent, Modal } from 'react-rails-pagination'
 import 'react-rails-pagination/dist/index.css'
 
 const App = () => {
-  return <ExampleComponent text="Create React Library Example 😄" />
+  const [isOpen, setIsOpen] = useState(false);
+  return(
+     <>
+        <button onClick={()=>{setIsOpen(!isOpen)}}>TEST</button>
+        <Modal element={<ElemForModal title={"title"}/>} state={[isOpen, setIsOpen]}/>
+        <h2>isOpen: {isOpen?"true":"false"}</h2>
+     </>
+  )
+}
+
+const ElemForModal = ({...props})=>{
+  const [value, setValue] = useState(0);
+  return (
+     <div className='ELement' style={{backgroundColor: "white", width: "100%", height: "100%"}}>
+        <h1>{props.title}</h1>
+        <p>value: {value}</p>
+        
+        <button onClick={()=>{setValue(value + 1)}}>+</button><button onClick={()=>{setValue(value - 1)}}>-</button>
+
+        <button onClick={()=>{setValue(0);props.closeModal();}}>save</button>
+     </div>
+  )
 }
 
 export default App
--- src/index.js
+++ src/index.js
@@ -1,6 +1,37 @@
 import React from 'react'
-import styles from './styles.module.css'
 
-export const ExampleComponent = ({ text }) => {
-  return <div className={styles.test}>Example Component: {text}</div>
+export const Modal = ({...props})=>{
+  const Element = React.cloneElement(props.element, {closeModal:()=>setIsOpen(false)});
+  const isOpen = props.state[0];
+  const setIsOpen = props.state[1];
+
+  return (
+     <div style={isOpen?{
+        display: "flex",
+        position: "absolute",
+        top:"0px",
+        left:"0px",
+        flexDirection: "column",
+        width: "100vw",
+        height: "100vh",
+        backgroundColor: "#00000063",
+        justifyContent: "center",
+        alignItems: "center"}:{display: "none"}}>
+       <div style={isOpen?{position: "absolute", width: "100vw", height: "100vh"}:{}} onClick={()=>{setIsOpen(false)}}></div>
+       <div style={{zIndex: "1",
+       width: "520px",
+       height: "550px",
+       justifyContent: "center",
+       alignItems: "center"}}>
+           <div style={{zIndex: "1",
+           display: "flex",
+           flexDirection: "column",
+           width: "520px",
+           height: "550px",
+           alignItems: "end"}}>
+              {Element}
+           </div>
+       </div>
+     </div>
+  );
 }
