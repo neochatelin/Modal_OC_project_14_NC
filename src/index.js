17b238427a3e59bc47dd5e48c6b60e6fe1a19031
neo chatelin <neo.chatelin@gmail.com>
2025-03-24 18:03:49 +0100

    commantaire

--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
 import React from 'react'
 
+/*
+   this component is a modal it allow you to put your element in it and with a state that you have created to open in and close it;
+*/
+
 export const Modal = ({...props})=>{
+  //when adding the modal you need to give a state and an element a new props is added to your element named closeModal a function that will change the value of the state you passed in;
   const Element = React.cloneElement(props.element, {closeModal:()=>setIsOpen(false)});
   const isOpen = props.state[0];
   const setIsOpen = props.state[1];
